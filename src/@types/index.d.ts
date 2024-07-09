4828d1dcea449dff7612eae1a73da320e2de1f57
Pascoal Kahamba <pascoalkahamba25@gmail.com>
2024-07-09 19:59:15 +0100

    Functinality to delete student and coordinator is done

--- src/@types/index.d.ts
+++ src/@types/index.d.ts
@@ -3,5 +3,8 @@ import { Admin, Student, Coordinator } from "@prisma/client";
 export type DataBaseExtraValues = "createdAt" | "updatedAt" | "id" | "role";
 
 export type TAdminModal = Omit<Admin, DataBaseExtraValues>;
+export type TAdminLogin = Pick<Admin, "email" | "password">;
+export type TKindsOfRole = "USER" | "ADMIN" | "COORDINATOR";
+
 export type TStudentModal = Omit<Student, DataBaseExtraValues>;
 export type TCoordinatorModal = Omit<Coordinator, DataBaseExtraValues>;
--- src/services/adminService.ts
+++ src/services/adminService.ts
@@ -1,8 +1,16 @@
-import { TAdminModal } from "../@types";
+import { TAdminLogin, TAdminModal } from "../@types";
 import { prismaService } from "./prismaService";
+import bcrypt from "bcrypt";
+
+const DEFAULT_SELECT = {
+  username: true,
+  email: true,
+  contact: true,
+};
 export default class AdminService {
   async create(adminModal: TAdminModal) {
     const { contact, email, username, password } = adminModal;
+    const hashPassword = await bcrypt.hash(password, 10);
 
     const admin = await prismaService.prisma.admin.findFirst({
       where: { email },
@@ -17,15 +25,120 @@ export default class AdminService {
         email,
         username,
         contact,
-        password,
+        password: hashPassword,
       },
+      select: DEFAULT_SELECT,
+    });
+
+    return adminCreated;
+  }
+
+  async login(adminInfo: TAdminLogin) {
+    const { email, password } = adminInfo;
+
+    const logged = await prismaService.prisma.admin.findFirst({
+      where: { email },
       select: {
         username: true,
+        password: true,
         email: true,
         contact: true,
       },
     });
 
-    return adminCreated;
+    if (!logged) {
+      return;
+    }
+
+    const samePassword = await bcrypt.compare(password, logged.password);
+
+    if (!samePassword) {
+      return;
+    }
+
+    const { password: _, ...admin } = logged;
+
+    return admin;
+  }
+
+  async updateInfo(adminModal: TAdminModal, id: number) {
+    const { username, password, contact, email } = adminModal;
+
+    const admin = await prismaService.prisma.admin.findFirst({
+      where: { id },
+    });
+
+    if (admin) {
+      return;
+    }
+
+    const adminUpdated = await prismaService.prisma.admin.update({
+      where: { id },
+      data: {
+        username,
+        password,
+        email,
+        contact,
+      },
+      select: DEFAULT_SELECT,
+    });
+
+    return adminUpdated;
+  }
+
+  async forgotPassword(email: string, newPassword: string) {
+    const hashPassword = await bcrypt.hash(newPassword, 10);
+
+    const admin = await prismaService.prisma.admin.findFirst({
+      where: { email },
+    });
+
+    if (!admin) {
+      return;
+    }
+
+    const passwordUpdated = await prismaService.prisma.admin.update({
+      where: { email },
+      data: {
+        password: hashPassword,
+      },
+      select: DEFAULT_SELECT,
+    });
+
+    return passwordUpdated;
+  }
+
+  async deleteCoordinator(email: string) {
+    const coordinator = await prismaService.prisma.coordinator.findFirst({
+      where: { email },
+    });
+
+    if (!coordinator) {
+      return;
+    }
+
+    const coordinatorDeleted = await prismaService.prisma.coordinator.delete({
+      where: { email },
+      select: DEFAULT_SELECT,
+    });
+
+    return coordinatorDeleted;
+  }
+
+  async deleteStudent(registrationNumber: number) {
+    const student = await prismaService.prisma.student.findFirst({
+      where: { registrationNumber },
+    });
+
+    if (!student) {
+      return;
+    }
+
+    const studentDeleted = await prismaService.prisma.student.delete({
+      where: { registrationNumber },
+      select: DEFAULT_SELECT,
+    });
+
+    return studentDeleted;
   }
 }
