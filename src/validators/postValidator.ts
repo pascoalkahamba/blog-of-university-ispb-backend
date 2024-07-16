9a283519a842c6a6ddac5a383bdca0b86b9211ea
Pascoal Kahamba <pascoalkahamba25@gmail.com>
2024-07-16 22:24:00 +0100

    I've just improved the code

--- src/validators/postValidator.ts
+++ src/validators/postValidator.ts
@@ -3,32 +3,72 @@ import { TPathError } from "../@types";
 import { handleError } from "../errors/handleError";
 import { PostError } from "../errors/postError";
 
-export default class AdminValidator {
+export default class PostValidator {
   validator(pathError: TPathError, res: Response) {
-    if (pathError === "username") {
-      return handleError(PostError.invalidInfo("Nome do admin invalido."), res);
-    }
-    if (pathError === "password") {
+    if (pathError === "content") {
       return handleError(
-        PostError.invalidInfo("Senha do admin invalido."),
+        PostError.invalidInfo("Conteudo do post invalido."),
         res
       );
     }
-    if (pathError === "email") {
+    if (pathError === "title") {
       return handleError(
-        PostError.invalidInfo("Email do admin invalido."),
+        PostError.invalidInfo("titulo do post invalido."),
         res
       );
     }
-    if (pathError === "contact") {
+    if (pathError === "kindOfFile") {
       return handleError(
-        PostError.invalidInfo("Contacto do admin invalido."),
+        PostError.invalidInfo("Tipo de arquivo invalido."),
+        res
+      );
+    }
+    if (pathError === "createrPostId") {
+      return handleError(PostError.invalidInfo("id do criador invalido."), res);
+    }
+
+    if (pathError === "width") {
+      return handleError(
+        PostError.invalidInfo("Tamanho do ficheiro invalido."),
+        res
+      );
+    }
+    if (pathError === "height") {
+      return handleError(
+        PostError.invalidInfo("Altura do ficheiro invalido."),
+        res
+      );
+    }
+    if (pathError === "mimeType") {
+      return handleError(
+        PostError.invalidInfo("Tipo do mimeType do ficheiro invalido."),
+        res
+      );
+    }
+    if (pathError === "size") {
+      return handleError(
+        PostError.invalidInfo("Tamanho do ficheiro invalido."),
         res
       );
     }
 
-    if (pathError === "emailAlreadyExist") {
-      return handleError(PostError.invalidInfo("Email já cadastrado."), res);
+    if (pathError === "nameOfDepartment") {
+      return handleError(
+        PostError.invalidInfo("Nome do departamento invalido."),
+        res
+      );
+    }
+    if (pathError === "whoPosted") {
+      return handleError(
+        PostError.invalidInfo("Nome do criador do post invalido."),
+        res
+      );
+    }
+    if (pathError === "name") {
+      return handleError(
+        PostError.invalidInfo("Nome do ficheiro invalido."),
+        res
+      );
     }
   }
 }
